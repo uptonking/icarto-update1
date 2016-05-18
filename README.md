ef1f2cac944987a23939134799773085cabfc968
uptonking <jinyaoo86@gmail.com>
2016-05-19 00:04:50 +0800

    initial commit for icarto

---
+++ README.md
@@ -0,0 +1,11 @@
+# icarto01
+iCarto Mapping v0.1  
+
+##Introduction
+    This repository is a windows forms application meant to faciliate map compilation, using ArcObjects & C#.  
+    The dev work is still in progress.  
+    
+##Disclaimer
+    I integrate several open source framework in my app.
+    If anybody find something *Inappropriate* in my work, please contact me at jinyaoo86@gmail.com.
+    I will reply as  soon as possible.
---
+++ iCarto/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+namespace iCarto
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Location = new System.Drawing.Point(422, 114);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(265, 265);
+            this.axMapControl1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 479);
+            this.Controls.Add(this.axMapControl1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+
+    }
+}
+
