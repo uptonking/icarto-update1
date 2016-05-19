87a3d4d5b022b01288161fce1643368956038098
uptonking <jinyaoo86@gmail.com>
2016-05-19 14:02:03 +0800

    首页切换完成

--- iCarto/MainForm.Designer.cs
+++ iCarto/MainForm.Designer.cs
@@ -28,21 +28,134 @@ namespace iCarto
         /// </summary>
         private void InitializeComponent()
         {
+            this.indexGroupBox = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.mainGroupBox = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.indexGroupBox.SuspendLayout();
+            this.mainGroupBox.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // indexGroupBox
+            // 
+            this.indexGroupBox.BackColor = System.Drawing.Color.Transparent;
+            this.indexGroupBox.Controls.Add(this.button6);
+            this.indexGroupBox.Controls.Add(this.button5);
+            this.indexGroupBox.Controls.Add(this.button4);
+            this.indexGroupBox.Controls.Add(this.button3);
+            this.indexGroupBox.Controls.Add(this.button1);
+            this.indexGroupBox.Location = new System.Drawing.Point(0, 29);
+            this.indexGroupBox.Margin = new System.Windows.Forms.Padding(0);
+            this.indexGroupBox.Name = "indexGroupBox";
+            this.indexGroupBox.Padding = new System.Windows.Forms.Padding(0);
+            this.indexGroupBox.Size = new System.Drawing.Size(784, 435);
+            this.indexGroupBox.TabIndex = 1;
+            this.indexGroupBox.TabStop = false;
+            this.indexGroupBox.Text = "indexGroupbox";
+            this.indexGroupBox.Paint += new System.Windows.Forms.PaintEventHandler(this.removeGroupBoxBorder_Paint);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(341, 181);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Middle";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(0, 407);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "BottomLeft";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(709, 407);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "BottomRight";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(709, 16);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "UpperRight";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "UpperLeft";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.swapBtnClick);
+            // 
+            // mainGroupBox
+            // 
+            this.mainGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainGroupBox.BackColor = System.Drawing.Color.Transparent;
+            this.mainGroupBox.Controls.Add(this.button2);
+            this.mainGroupBox.Location = new System.Drawing.Point(-800, 29);
+            this.mainGroupBox.Name = "mainGroupBox";
+            this.mainGroupBox.Size = new System.Drawing.Size(800, 430);
+            this.mainGroupBox.TabIndex = 2;
+            this.mainGroupBox.TabStop = false;
+            this.mainGroupBox.Text = "mainGroupBox";
+            this.mainGroupBox.Paint += new System.Windows.Forms.PaintEventHandler(this.removeGroupBoxBorder_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.swapBtnClick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(827, 479);
+            this.ClientSize = new System.Drawing.Size(784, 464);
+            this.Controls.Add(this.mainGroupBox);
+            this.Controls.Add(this.indexGroupBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
             this.Name = "MainForm";
             this.Title = "Form1";
+            this.Controls.SetChildIndex(this.indexGroupBox, 0);
+            this.Controls.SetChildIndex(this.mainGroupBox, 0);
+            this.indexGroupBox.ResumeLayout(false);
+            this.mainGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox indexGroupBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox mainGroupBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
 
 
     }
--- iCarto/MainForm.cs
+++ iCarto/MainForm.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Transitions;
 
 namespace iCarto
 {
@@ -15,5 +16,56 @@ namespace iCarto
         {
             InitializeComponent();
         }
+
+        private void swapBtnClick(object sender, EventArgs e)
+        {
+            uiSwap();
+        }
+
+        private void removeGroupBoxBorder_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            e.Graphics.Clear(this.BackColor);
+        }
+
+ 
+
+        /// <summary>
+        /// Called when the "Swap" button is pressed.
+        /// </summary>
+        private void uiSwap()
+        {
+            // We swap over the group-boxes that show the "Bounce" and 
+            // "Throw and Catch" transitions. The active one is animated 
+            // left off the screen and the inactive one is animated right
+            // onto the screen...
+
+            // We work out which box is currently on screen and
+            // which is off screen...
+            Control ctrlOnScreen, ctrlOffScreen;
+            if (indexGroupBox.Left == 0)
+            {
+                ctrlOnScreen = indexGroupBox;
+                ctrlOffScreen = mainGroupBox;
+            }
+            else
+            {
+                ctrlOnScreen = mainGroupBox;
+                ctrlOffScreen = indexGroupBox;
+            }
+            ctrlOnScreen.SendToBack();
+            ctrlOffScreen.BringToFront();
+
+            // We create a transition to animate the two boxes simultaneously. One is
+            // animated onto the screen, the other off the screen.
+
+            // The ease-in-ease-out transition acclerates the rate of change for the 
+            // first half of the animation, and decelerates during the second half.
+
+            Transition t = new Transition(new TransitionType_EaseInEaseOut(500));
+            t.add(ctrlOnScreen, "Left", -1 * ctrlOnScreen.Width);
+            t.add(ctrlOffScreen, "Left", 0);
+            t.run();
+        }
+
     }
 }
